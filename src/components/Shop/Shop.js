12f78f7f835c6842c6c0dd5d74164c78748d8a75
Shagor <mdshagormahamud028@gmail.com>
2022-09-27 14:53:25 +0600

    added by cart resolve

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,18 +4,45 @@ import { useState } from 'react';
 import './Shop.css'
 import ProductData from '../ProductData/ProductData';
 import Cart from '../Cart/Cart';
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 const Shop = () => {
-    const [products, setShops] = useState([]);
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
-            .then(data => setShops(data))
+            .then(data => setProducts(data))
     }, [])
-    const handleAddToCart = (product) => {
-        console.log(product)
-        const newCart = [...cart, product];
+
+    useEffect(() => {
+        const StoredCart = getStoredCart()
+        const saveCart = []
+        for (const id in StoredCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = StoredCart[id]
+                addedProduct.quantity = quantity
+                saveCart.push(addedProduct)
+            }
+            setCart(saveCart)
+        }
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) => {
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        let newCart = [];
+        if (!exists) {
+            selectedProduct.quantity = 1
+            newCart = [...cart, selectedProduct]
+        }
+        else {
+            const res = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity + 1;
+            newCart = [...res, exists]
+        }
         setCart(newCart)
+        addToDb(selectedProduct.id)
     }
     return (
         <div className='shop'>
